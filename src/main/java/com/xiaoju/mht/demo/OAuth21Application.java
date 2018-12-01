957a0620969bd58e388b59f974fb7b02e597d316
hetianyun <hetainyun@bupt.edu.cn>
2018-12-02 00:46:22 +0800

    first commit

---
+++ src/main/java/com/xiaoju/mht/demo/OAuth21Application.java
@@ -0,0 +1,12 @@
+package com.xiaoju.mht.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth21Application.class, args);
+	}
+}
---
+++ src/main/java/com/xiaoju/mht/demo/config/OAuth2AuthorizationServer.java
@@ -0,0 +1,27 @@
+package com.xiaoju.mht.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by hetianyun on 2018/12/2.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends
+    AuthorizationServerConfigurerAdapter {
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.inMemory()
+        .withClient("clientapp")
+        .secret("112233")
+        .redirectUris("http://localhost:9001/callback")
+        //简化模式
+        .authorizedGrantTypes("implicit")
+        .accessTokenValiditySeconds(120)
+        .scopes("read_userinfo", "read_contracts");
+
+  }
+}
